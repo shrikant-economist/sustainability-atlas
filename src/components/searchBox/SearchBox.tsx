7a4c0ff1f22293e6d330465d078a3436d00e69a0
Aniket Gupta <aniketgupta@partner.economist.com>
2024-08-13 13:34:08 +0530

    search_box

---
+++ src/components/searchBox/SearchBox.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import './SearchBox.css';
+import { ReactComponent as SendIcon } from '../../assets/icons/SendIcon.svg';
+import { ReactComponent as CrossBtn } from '../../assets/icons/CrossBtn.svg';
+import { ReactComponent as ResetIcon } from '../../assets/icons/ResetBtn.svg';
+import { ReactComponent as QuestionMark } from '../../assets/icons/QuestionMark.svg';
+const TextField = React.lazy(() => import('@mui/material/TextField'));
+const InputAdornment = React.lazy(() => import('@mui/material/InputAdornment'));
+
+interface SearchBoxProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  handleSearch: () => void;
+  ShowResetBtn: boolean;
+  handleReset: () => void;
+}
+
+const SearchBox: React.FC<SearchBoxProps> = ({
+  value,
+  onChange,
+  handleSearch,
+  ShowResetBtn,
+  handleReset,
+}) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
+
+  const handleCross = () => {
+    onChange({
+      target: { value: '' },
+    } as React.ChangeEvent<HTMLTextAreaElement>);
+  };
+  return (
+    <div className="search_box_div">
+      {ShowResetBtn && <ResetIcon onClick={handleReset} />}
+      <TextField
+        variant="standard"
+        value={value}
+        onChange={onChange}
+        fullWidth
+        className="search_box"
+        InputProps={{
+          disableUnderline: true,
+          endAdornment: (
+            <InputAdornment position="end">
+              {value != '' && (
+                <div className="icon_div">
+                  <CrossBtn className="cursor_pointer" onClick={handleCross} />
+                  <SendIcon className="cursor_pointer" onClick={handleSearch} />
+                </div>
+              )}
+            </InputAdornment>
+          ),
+        }}
+        placeholder={
+          matches
+            ? 'Ask your sustainability or business problem question'
+            : 'Ask your sustainability question'
+        }
+        multiline
+        maxRows={5}
+      />
+      <span className="help_text_span">
+        Data caveat here to remind users this is an AI, not advice lorem ipsum
+        dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+        euismod tincidunt ut laoreet dolore magna <QuestionMark />
+      </span>
+    </div>
+  );
+};
+
+export default SearchBox;
