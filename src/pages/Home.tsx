90563c8391a1ce51d5d5c92743fc13e476d92fe4
Himanshu Pal <HimanshuPal@partner.economist.com>
2024-08-20 19:31:06 +0530

    delete unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
-import React, { lazy } from 'react';
+import { lazy } from 'react';
 import { useTranslation } from 'react-i18next';
 import imageUrl from './../assets/images/Intro 2.svg';
 
 const AtlasImage = lazy(() => import('./../components/AtlasImage'));
-const AtlasText = lazy(() => import('../components/atlasText/AtlasText'));
-const AtlasTextBox = lazy(() => import('../components/atlasTextBox/AtlasTextBox'));
 const AtlasTitleText = lazy(() => import('../components/atlasTitleText/AtlasTitleText'));
-const AtlasFooterText = lazy(() => import('../components/atlasFooterText/AtlasFooterText'));
 
 const Home = () => {
   const { t } = useTranslation();
